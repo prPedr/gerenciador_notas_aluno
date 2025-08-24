66d9c6e85ea7b862a667aa8ee011cdac9b2fa8d9
prPedr <prpedro025@gmail.com>
2025-08-24 09:57:06 -0300

    Metodos PUT e DELELTE inseridos

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const servidor = http.createServer((request, response) => {
     })
 
     request.on("end", () => {
+        const id = url.split("/")[2]
+
         if (url === "/notas" && method === "GET") {
             response.writeHead(200, {"Content-Type": "application/json"}) // Para um GET o statusCode padrao e o 200
             response.end(JSON.stringify(notas))
@@ -23,6 +25,29 @@ const servidor = http.createServer((request, response) => {
             notas.push(novaNota)
             response.writeHead(201, {"Content-Type": "application/json"})
             response.end(JSON.stringify(novaNota))
+        } else if (url.startsWith("/notas/") && method === "PUT") {
+            const {nomeEstudante, materia, nota} = JSON.parse(body)
+            const notaAtualizar = notas.find(notaFiltro => notaFiltro.id === id)
+            if (notaAtualizar) {
+                notaAtualizar.nomeEstudante = nomeEstudante
+                notaAtualizar.materia = materia
+                notaAtualizar.nota = nota
+                response.writeHead(200, {"Content-Type": "application/json"})
+                response.end(JSON.stringify(notaAtualizar))
+            } else {
+                response.writeHead(404, {"Content-Type": "application/json"})
+                response.end(JSON.stringify({mensagem: "Nota não encontrada"}))
+            }
+        } else if (url.startsWith("/notas") && method === "DELETE") {
+            const index = notas.findIndex(notaFiltro => notaFiltro.id === id)
+            if (index !== -1) {
+                notas.splice(index, 1)
+                response.writeHead(204)
+                response.end()
+            } else {
+                response.writeHead(404, {"Content-Type": "application/json"})
+                response.end(JSON.stringify({mensagem: "Nota não encontrada"}))
+            }
         } else {
             response.writeHead(404, {"Content-Type": "application/json"})
             response.end(JSON.stringify({mensagem: "Rota não encontrada"}))
