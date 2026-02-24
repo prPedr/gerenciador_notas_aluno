c1eb5d78a8ac68d8cb427835ab60edb5690ea59b
PedroRodrigues <prpedro025@gmail.com>
2026-02-24 04:43:42 -0300

    Implementando uma rota GET na aplicacao

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 import http from "http"
 
+const notas_alunos = [
+  {
+    "nomeEstudante": "Pedro",
+    "materia": "Back end com node js",
+    "nota": "10"
+  }
+]
+
 const server = http.createServer((request, response) => {
-  response.end("Hello World!")
+  const {method, url} = request
+
+  if (url === "/notas" && method === "GET") {
+    response.writeHead(200, {"content-type": "application/json"})
+    response.end(JSON.stringify(notas_alunos))
+  } else {
+    response.writeHead(404, {"content-type": "application/json"})
+    response.end(JSON.stringify({message: "Rota nao encontrada"}))
+  }
 })
 
 const porta = 3333
