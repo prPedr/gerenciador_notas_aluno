a9539f7ac1c8cfff1583962a2123708c2fa97c48
PedroRodrigues <prpedro025@gmail.com>
2026-02-24 04:17:44 -0300

    Reescrevendo o index.js para revisao

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import http from "http"
 
-const porta = 3333
-
 const server = http.createServer((request, response) => {
   response.end("Hello World!")
 })
 
+const porta = 3333
+
 server.listen(porta, () => {
-  console.log(`Servidor rodando http://localhost:${porta}/`)
+  console.log(`Servidor rodando em http://localhost:${porta}/`)
 })
