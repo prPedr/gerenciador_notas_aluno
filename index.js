16a8d4d2c06efbc3b2ecfaa83eb4675472c57ce7
PedroRodrigues <prpedro025@gmail.com>
2026-02-24 05:37:45 -0300

    Criacao de uma rota post

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import http from "http"
+import {v4} from "uuid"
 
-const notasAluno = [
+const informacoesAlunos = [
   {
     "nomeAluno": "Pedro",
     "materiaAluno": "Back-end com node js",
@@ -11,13 +12,27 @@ const notasAluno = [
 const server = http.createServer((request, response) => {
   const {method, url} = request
 
-  if (url === "/notas" && method === "GET") {
-    response.writeHead(200, {"content-type":"application/json"})
-    response.end(JSON.stringify(notasAluno))
-  } else {
-    response.writeHead(404, {"content-type":"application/json"})
-    response.end(JSON.stringify({Mensagem: "Rota nao encontrada"}))
-  }
+  let body = ""
+
+  request.on("data", dadosAluno => {
+    body += dadosAluno.toString()
+  })
+
+  request.on("end", () => {
+    if (url === "/notas" && method === "GET") {
+      response.writeHead(200, {"content-type":"application/json"})
+      response.end(JSON.stringify(informacoesAlunos))
+    } else if (url === "/notas" && method === "POST") {
+      const {nomeAluno, materiaAluno, notaAluno} = JSON.parse(body)
+      const novaInformacaoAluno = {id: v4(), nomeAluno, materiaAluno, notaAluno}
+      informacoesAlunos.push(novaInformacaoAluno)
+      response.writeHead(201, {"content-type":"application/json"})
+      response.end(novaInformacaoAluno)
+    } else {
+      response.writeHead(404, {"content-type":"application/json"})
+      response.end(JSON.stringify({Mensagem: "Rota nao encontrada"}))
+    }
+  })
 })
 
 const porta = 3333
