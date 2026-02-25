1212e068d1b3fe8a1b25ab29aabd30f385e43c87
PedroRodrigues <prpedro025@gmail.com>
2026-02-25 04:41:28 -0300

    Adicionando comentarios para um melhor entendimento do .on

--- index.js
+++ index.js
@@ -14,21 +14,34 @@ const server = http.createServer((request, response) => {
 
   let body = ""
 
+  // 1. Ouve o evento de chegada de dados. 
+  // O Node recebe "chunks" (pedaços) e os concatena na variável body.
   request.on("data", dadosAluno => {
     body += dadosAluno.toString()
   })
 
+  // 2. Ouve o evento de término da requisição.
+  // Tudo o que depende do corpo (body) da mensagem deve estar aqui dentro.
   request.on("end", () => {
+    
+    // Rota para listar notas
     if (url === "/notas" && method === "GET") {
       response.writeHead(200, {"content-type":"application/json"})
       response.end(JSON.stringify(informacoesAlunos))
-    } else if (url === "/notas" && method === "POST") {
+    } 
+    
+    // Rota para adicionar nova nota
+    else if (url === "/notas" && method === "POST") {
       const {nomeAluno, materiaAluno, notaAluno} = JSON.parse(body)
       const novaInformacaoAluno = {id: v4(), nomeAluno, materiaAluno, notaAluno}
-      informacoesAlunos.push(novaInformacaoAluno)
+      
+      informacoesAlunos.push(novaInformacaoAluno)   
       response.writeHead(201, {"content-type":"application/json"})
-      response.end(novaInformacaoAluno)
-    } else {
+      response.end(JSON.stringify(novaInformacaoAluno))
+    } 
+    
+    // Tratamento de rota não encontrada
+    else {
       response.writeHead(404, {"content-type":"application/json"})
       response.end(JSON.stringify({Mensagem: "Rota nao encontrada"}))
     }
