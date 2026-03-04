102a29c620c3483bdef01260549eae7c7bda99f8
prPedr <prpedro025@gmail.com>
2026-03-04 05:47:25 -0300

    Reescrevendo o POST da aplicacao

--- index.js
+++ index.js
@@ -12,16 +12,43 @@ const informacoesAlunos = [
 
 const server = http.createServer((request, response) => {
   const { method, url } = request
-  
-  if (method === "GET" && url === "/alunos") {
-    response.writeHead(200, {"content-type":"application/json"})
-    response.end(JSON.stringify(informacoesAlunos))
-  }
+  let body = ""
 
-  else {
-    response.writeHead(404, {"content-type":"application/jsop"})
-    response.end(JSON.stringify({Mensagem: "Rota nao encontrada"}))
-  }
+  request.on("data", partesRequisicao => {
+    body += partesRequisicao.toString()
+  })
+
+  request.on("end", () => {
+    // ROTA DE LISTAGEM
+    if (method === "GET" && url === "/alunos") {
+      response.writeHead(200, {"content-type":"application/json"})
+      response.end(JSON.stringify(informacoesAlunos))
+    }
+
+    // ROTA DE CRIACAO
+    else if (method === "POST" && url === "/alunos") {
+      const { nomeAluno, materiaAluno, notaAluno } = JSON.parse(body)
+
+      const novasInformacoesAlunos = {
+        id: randomUUID(),
+        nomeAluno,
+        materiaAluno,
+        notaAluno
+      }
+
+      informacoesAlunos.push(novasInformacoesAlunos)
+      response.writeHead(201, {"content-type":"application/json"})
+      response.end(JSON.stringify(novasInformacoesAlunos))
+    }
+
+    // ROTA URL NAO ENCONTRADA
+    else {
+      response.writeHead(404, {"content-type":"application/jsop"})
+      response.end(JSON.stringify({Mensagem: "Rota nao encontrada"}))
+    }
+  })
+  
+  
 })
 
 const porta = 3333
