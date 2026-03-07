b3e38b7bba4cb8abd52e37ce62f41ca3c4df08d4
prPedr <prpedro025@gmail.com>
2026-03-07 15:06:06 -0300

    Reescrevendo o metodo get

--- index.js
+++ index.js
@@ -3,50 +3,25 @@ import { randomUUID } from "node:crypto"
 
 const informacoesAlunos = [
   {
-    "id": randomUUID(),
-    "nomeAluno": "Receputi",
-    "materiaAluno": "Introducao ao Node JS",
-    "notaAluno": "10"
+    "idAluno": randomUUID(),
+    "nomeAluno" : "Pedro",
+    "materiaAluno" : "Back end com node JS",
+    "notaAluno" : "10"
   }
 ]
 
 const server = http.createServer((request, response) => {
   const { method, url } = request
-  let body = ""
 
-  request.on("data", partesRequisicao => {
-    body += partesRequisicao.toString()
-  })
+  if (url === "/alunos" && method === "GET") {
+    response.writeHead(200, {"content-type" : "application/json"})
+    response.end(JSON.stringify(informacoesAlunos))
+  }
 
-  request.on("end", () => {
-    // ROTA DE LISTAGEM
-    if (method === "GET" && url === "/alunos") {
-      response.writeHead(200, {"content-type":"application/json"})
-      response.end(JSON.stringify(informacoesAlunos))
-    }
-
-    // ROTA DE CRIACAO
-    else if (method === "POST" && url === "/alunos") {
-      const { nomeAluno, materiaAluno, notaAluno } = JSON.parse(body)
-
-      const novasInformacoesAlunos = {
-        id: randomUUID(),
-        nomeAluno,
-        materiaAluno,
-        notaAluno
-      }
-
-      informacoesAlunos.push(novasInformacoesAlunos)
-      response.writeHead(201, {"content-type":"application/json"})
-      response.end(JSON.stringify(novasInformacoesAlunos))
-    }
-
-    // ROTA URL NAO ENCONTRADA
-    else {
-      response.writeHead(404, {"content-type":"application/jsop"})
-      response.end(JSON.stringify({Mensagem: "Rota nao encontrada"}))
-    }
-  })
+  else {
+    response.writeHead(404, {"content-type" : "application/json"})
+    response.end(JSON.stringify({Mensagem: "Rota nao encontrada"}))
+  }
 })
 
 const porta = 3333
