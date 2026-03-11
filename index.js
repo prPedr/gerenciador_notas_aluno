6257dc06493ad85c46e997718cb6ddabebfb5d9b
prPedr <prpedro025@gmail.com>
2026-03-11 05:06:55 -0300

    Revisao : Reescrevendo o metodo GET

--- index.js
+++ index.js
@@ -1,92 +1,31 @@
 import http from "node:http"
 import { randomUUID } from "node:crypto"
 
-const informacoesAlunos = [
+const informacoesAluno = [
   {
     "idAluno" : randomUUID(),
-    "nomeAluno" : "Matheus Miguel",
-    "materiaAluno" : "Introducao ao React",
+    "nomeAluno" : "Pedro Nascimento",
+    "materiaAluno" : "Back end com Node.js",
     "notaAluno" : "10"
   }
 ]
 
 const server = http.createServer((request, response) => {
   const { method, url } = request
-  let body = ""
 
-  request.on("data", partesRequesicao => {
-    body += partesRequesicao
-  })
+  if (url === "/alunos" && method === "GET") {
+    response.writeHead(200, {"content-type" : "application/json"})
+    response.end(JSON.stringify(informacoesAluno))
+  }
 
-  request.on("end", () => {
-    const idBuscaAluno = url.split("/")[2]
-
-    // ROTA DE LISTAGEM
-    if (url === "/alunos" && method === "GET") {
-      response.writeHead(200, {"content-type" : "application/json"})
-      response.end(JSON.stringify(informacoesAlunos))
-    }
-
-    // ROTA DE CRIACAO
-    else if (url === "/alunos" && method === "POST") {
-      const { nomeAluno, materiaAluno, notaAluno } = JSON.parse(body)
-
-      const novasInformacoesAlunos = {
-        idAluno : randomUUID(),
-        nomeAluno,
-        materiaAluno,
-        notaAluno
-      }
-
-      informacoesAlunos.push(novasInformacoesAlunos)
-      response.writeHead(201, {"content-type" : "application/json"})
-      response.end(JSON.stringify(novasInformacoesAlunos))
-    }
-
-    // ROTA DE ATUALIZACAO
-    else if (url.startsWith("/alunos/") && method === "PUT") {
-      const { nomeAluno, materiaAluno, notaAluno } = JSON.parse(body)
-      const atualizarAluno = informacoesAlunos.find(aluno => aluno.idAluno === idBuscaAluno)
-
-      if (atualizarAluno) {
-        atualizarAluno.nomeAluno = nomeAluno
-        atualizarAluno.materiaAluno = materiaAluno
-        atualizarAluno.notaAluno = notaAluno
-
-        response.writeHead(200, {"content-type" : "äpplication/json"})
-        response.end(JSON.stringify(atualizarAluno))
-      } else {
-        response.writeHead(404, {"content-type" : "application/json"})
-        response.end(JSON.stringify({Mensagem : "Aluno nao encontrado"}))
-      }
-    }
-
-    // ROTA DE DELETAR
-    else if (url.startsWith("/alunos/") && method === "DELETE") {
-      const deletarAluno = informacoesAlunos.findIndex(aluno => aluno.idAluno === idBuscaAluno)
-
-      if (deletarAluno !== -1) {
-        const nomeAlunoDeletado = informacoesAlunos[deletarAluno].nomeAluno
-
-        informacoesAlunos.splice(deletarAluno, 1)
-        response.writeHead(200, {"content-type" : "application/json"})
-        response.end(JSON.stringify({Mensagem : `${nomeAlunoDeletado} deletado`}))
-      } else {
-        response.writeHead(404, {"content-type" : "application/json"})
-        response.end(JSON.stringify({Mensagem : "Aluno nao encontrado"}))
-      }
-    }
-
-    // ROTA DE URL NAO ENCONTRADA
-    else {
-      response.writeHead(404, {"content-type" : "application'json"})
-      response.end(JSON.stringify({Mensagem : "Rota nao encontrada"}))
-    }
-  })
+  else {
+    response.writeHead(404, {"content-type" : "application/json"})
+    response.end(JSON.stringify({Mensagem : "Rota nao encontrada"}))
+  }
 })
 
-const porta = 3333
+const porta = 3111
 
 server.listen(porta, () => {
-  console.log(`Servidor rodando em http://localhost:${porta}`)
+  console.log(`Servido rodando em http://localhost:${porta}`)
 })
